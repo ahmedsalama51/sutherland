8b5eb31dc32689946a2b1d31e38e118ac131c52e
ahmed <ahmed.salama1679@gmail.com>
2020-10-20 22:10:44 +0200

    [CHANGE] notes that is on sheets has been done

--- egymentors_hr_tax_slides/models/hr_tax_slide.py
+++ egymentors_hr_tax_slides/models/hr_tax_slide.py
@@ -6,7 +6,7 @@ from odoo.exceptions import Warning
 
 class HrTaxSlide(models.Model):
     _name = 'hr.tax.slide'
-    _inherit = 'hr.payslip'
+    # _inherit = 'hr.payslip'
     _description = "HR Tax Slide"
     _inherit = ['mail.thread', 'image.mixin']
     _order = 'priority'
@@ -38,11 +38,11 @@ class HrTaxSlide(models.Model):
                               % (slc.priority, slc.max_amount))
 
     def compute_tax_amount(self, amount):
-        # print("HERE: ", amount, self)
+        print("HERE: ", amount, self)
         total_tax = 0.0
         for slide in self.sorted(lambda s: s.priority):
-            # print("SLIDE: ", slide.max_amount)
-            if amount >= slide.max_amount:
+            print("SLIDE: ", slide.max_amount)
+            if amount <= slide.max_amount:
                 # print("--------- start ----------------")
                 # start to process amount
                 process_amount = amount
@@ -50,98 +50,100 @@ class HrTaxSlide(models.Model):
                 # print("process_amount: ", process_amount)
                 for line in slide.line_ids:
                     # print("-------------\nCOND: ", line.amount_from, process_amount, line.amount_to)
-                    if process_amount >= line.amount_to:
-                        tax = (line.amount_to * line.tax_percentage / 100)
+                    if process_amount >= line.line_amount:
+                        tax = (line.line_amount * line.tax_percentage / 100)
                         # print("TAX: ", tax)
                         slide_taxes += tax
                         process_amount -= line.line_amount
+                    else:
+                        slide_taxes += (process_amount * line.tax_percentage / 100)
+                        break
                 total_tax += slide_taxes
         # print("total_tax: ", total_tax)
         return total_tax
 
 # Tax Compute function
-    def taxesStageLoop(self, netSalary):
-        taxedAmount = netSalary
-        perRound = 0
-        if taxedAmount < self.max_amount:
-            for i in range(self.line_ids):
-                if i == 0:
-                    if taxedAmount <= i.line_amount:
-                        taxedAmount = 0
-                        i.level = 0
-                        perRound = 0
-                    elif taxedAmount >= i.line_amount:
-                        taxedAmount = taxedAmount - i.line_amount
-                        i.level = 0
-                        perRound = 0
-                elif i == 1:
-                    if taxedAmount >= i.line_amount:
-                        taxedAmount = taxedAmount - i.line_amount
-                        i.level = 1
-                        perRound = 1
-                elif i == 2:
-                    if taxedAmount >= i.line_amount:
-                        taxedAmount = taxedAmount - i.line_amount
-                        i.level = 2
-                        perRound = 2
-                elif i == 3:
-                    if taxedAmount >= i.line_amount:
-                        taxedAmount = taxedAmount - i.line_amount
-                        i.level = 3
-                        perRound = 3
-                elif i == 4:
-                    if taxedAmount >= i.line_amount:
-                        taxedAmount = taxedAmount - i.line_amount
-                        i.level = 4
-                        perRound = 4
-                elif i == 5:
-                    if taxedAmount >= i.line_amount:
-                        taxedAmount = taxedAmount - i.line_amount
-                        i.level = 5
-                        perRound = 5
-            taxedAmount = round(taxedAmount, 2)
-            return taxedAmount, perRound
-
-    def calculatedTotalTaxes(self ,taxedAmount , perRound):
-        tax = 0.00
-        if perRound == 0 and taxedAmount == 0:
-            tax = 0
-        elif perRound == 0 and taxedAmount != 0:
-            level = self.env['hr.tax.slide.line'].search([('level', '=', perRound+1)], limit=1)
-            tax = taxedAmount * (level.tax_percentage/100)
-        elif perRound == 1 and taxedAmount ==0:
-            level = self.env['hr.tax.slide.line'].search([('level','=',perRound)], limit=1)
-            tax = level.amount * (level.tax_percentage/100)
-        elif perRound == 1 and taxedAmount !=0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound+1)])
-            tax = level[1].amount * (level[1].tax_percentage/100) + taxedAmount * (level[2].tax_percentage/100)
-        elif perRound == 2 and taxedAmount ==0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound)])
-            tax = level[1].amount * (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100)
-        elif perRound == 2 and taxedAmount !=0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound+1)])
-            tax = level[1].amount* (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + taxedAmount * (level[3].tax_percentage/100)
-        elif perRound == 3 and taxedAmount == 0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound)])
-            tax = level[1].amount * (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].tax_percentage/100)
-        elif perRound == 3 and taxedAmount !=0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound + 1)])
-            tax = level[1].amount* (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].tax_percentage/100) + taxedAmount * (level[4].tax_percentage/100)
-        elif perRound == 4 and taxedAmount ==0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound)])
-            tax = level[1].amount* (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].tax_percentage/100) + level[4].amount * (level[4].tax_percentage/100)
-        elif perRound == 4 and taxedAmount !=0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound+1)])
-            tax = level[1].amount* (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].taxed_percentage/100) + level[4].amount * (level[4].taxed_percentage/100)+ taxedAmount * (level[5].tax_percentage/100)
-        elif perRound == 5 and taxedAmount ==0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound)])
-            tax = level[1].amount * (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].taxed_percentage/100) + level[4].amount * (level[4].taxed_percentage/100) + level[5].amount * level[5].tax_percentage
-        elif perRound == 5 and taxedAmount !=0:
-            level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound+1)])
-            tax = level[1].amount * (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].tax_percentage/100) + level[4].amount * (level[4].tax_percentage/100) + level[5].amount * (level[5].tax_percentage/100) + taxedAmount * (level[6].tax_percentage/100)
-
-        return round(tax,2)
-
+#     def taxesStageLoop(self, netSalary):
+#         taxedAmount = netSalary
+#         perRound = 0
+#         if taxedAmount < self.max_amount:
+#             for i in range(self.line_ids):
+#                 if i == 0:
+#                     if taxedAmount <= i.line_amount:
+#                         taxedAmount = 0
+#                         i.level = 0
+#                         perRound = 0
+#                     elif taxedAmount >= i.line_amount:
+#                         taxedAmount = taxedAmount - i.line_amount
+#                         i.level = 0
+#                         perRound = 0
+#                 elif i == 1:
+#                     if taxedAmount >= i.line_amount:
+#                         taxedAmount = taxedAmount - i.line_amount
+#                         i.level = 1
+#                         perRound = 1
+#                 elif i == 2:
+#                     if taxedAmount >= i.line_amount:
+#                         taxedAmount = taxedAmount - i.line_amount
+#                         i.level = 2
+#                         perRound = 2
+#                 elif i == 3:
+#                     if taxedAmount >= i.line_amount:
+#                         taxedAmount = taxedAmount - i.line_amount
+#                         i.level = 3
+#                         perRound = 3
+#                 elif i == 4:
+#                     if taxedAmount >= i.line_amount:
+#                         taxedAmount = taxedAmount - i.line_amount
+#                         i.level = 4
+#                         perRound = 4
+#                 elif i == 5:
+#                     if taxedAmount >= i.line_amount:
+#                         taxedAmount = taxedAmount - i.line_amount
+#                         i.level = 5
+#                         perRound = 5
+#             taxedAmount = round(taxedAmount, 2)
+#             return taxedAmount, perRound
+#
+#     def calculatedTotalTaxes(self ,taxedAmount , perRound):
+#         tax = 0.00
+#         if perRound == 0 and taxedAmount == 0:
+#             tax = 0
+#         elif perRound == 0 and taxedAmount != 0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '=', perRound+1)], limit=1)
+#             tax = taxedAmount * (level.tax_percentage/100)
+#         elif perRound == 1 and taxedAmount ==0:
+#             level = self.env['hr.tax.slide.line'].search([('level','=',perRound)], limit=1)
+#             tax = level.amount * (level.tax_percentage/100)
+#         elif perRound == 1 and taxedAmount !=0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound+1)])
+#             tax = level[1].amount * (level[1].tax_percentage/100) + taxedAmount * (level[2].tax_percentage/100)
+#         elif perRound == 2 and taxedAmount ==0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound)])
+#             tax = level[1].amount * (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100)
+#         elif perRound == 2 and taxedAmount !=0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound+1)])
+#             tax = level[1].amount* (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + taxedAmount * (level[3].tax_percentage/100)
+#         elif perRound == 3 and taxedAmount == 0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound)])
+#             tax = level[1].amount * (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].tax_percentage/100)
+#         elif perRound == 3 and taxedAmount !=0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound + 1)])
+#             tax = level[1].amount* (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].tax_percentage/100) + taxedAmount * (level[4].tax_percentage/100)
+#         elif perRound == 4 and taxedAmount ==0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound)])
+#             tax = level[1].amount* (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].tax_percentage/100) + level[4].amount * (level[4].tax_percentage/100)
+#         elif perRound == 4 and taxedAmount !=0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound+1)])
+#             tax = level[1].amount* (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].taxed_percentage/100) + level[4].amount * (level[4].taxed_percentage/100)+ taxedAmount * (level[5].tax_percentage/100)
+#         elif perRound == 5 and taxedAmount ==0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound)])
+#             tax = level[1].amount * (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].taxed_percentage/100) + level[4].amount * (level[4].taxed_percentage/100) + level[5].amount * level[5].tax_percentage
+#         elif perRound == 5 and taxedAmount !=0:
+#             level = self.env['hr.tax.slide.line'].search([('level', '<=', perRound+1)])
+#             tax = level[1].amount * (level[1].tax_percentage/100) + level[2].amount * (level[2].tax_percentage/100) + level[3].amount * (level[3].tax_percentage/100) + level[4].amount * (level[4].tax_percentage/100) + level[5].amount * (level[5].tax_percentage/100) + taxedAmount * (level[6].tax_percentage/100)
+#
+#         return round(tax,2)
 
 
 class HrTaxSlideLine(models.Model):
@@ -154,7 +156,7 @@ class HrTaxSlideLine(models.Model):
     amount_to = fields.Monetary("Amount To", required=True)
     line_amount = fields.Monetary("Amount", compute='_compute_line_amount')
     tax_percentage = fields.Float("Tax Percentage(%)", required=True)
-    level = fields.Integer("Tax Level")
+    # level = fields.Integer("Tax Level")
     currency_id = fields.Many2one(related='tax_slide_id.currency_id')
     comment = fields.Text("Comment")
 
